53cf2902a8863aaec26bf36f6d05c9465c0238c8
anthonyloaiza56 <lap201197@hotmail.com>
2017-09-27 02:03:12 -0600

    Cliente rm con interfaz
    Se tiene una interfaz que obtiene los valores ingresados para puertos e ip's, y tambien llave-valor a enviar al servidor activo

--- Ui_2/Ui_2Main.cpp
+++ Ui_2/Ui_2Main.cpp
@@ -10,6 +10,7 @@
 #include "Ui_2Main.h"
 #include <wx/msgdlg.h>
 #include "rmlib.h"
+#include <string.h>
 
 //(*InternalHeaders(Ui_2Frame)
 #include <wx/string.h>
@@ -64,6 +65,10 @@ const long Ui_2Frame::ID_TEXTCTRL5 = wxNewId();
 const long Ui_2Frame::ID_TEXTCTRL6 = wxNewId();
 const long Ui_2Frame::ID_STATICTEXT4 = wxNewId();
 const long Ui_2Frame::ID_STATICTEXT5 = wxNewId();
+const long Ui_2Frame::ID_BUTTON8 = wxNewId();
+const long Ui_2Frame::ID_BUTTON9 = wxNewId();
+const long Ui_2Frame::ID_TEXTCTRL7 = wxNewId();
+const long Ui_2Frame::ID_TEXTCTRL8 = wxNewId();
 const long Ui_2Frame::idMenuQuit = wxNewId();
 const long Ui_2Frame::idMenuAbout = wxNewId();
 const long Ui_2Frame::ID_STATUSBAR1 = wxNewId();
@@ -94,20 +99,24 @@ Ui_2Frame::Ui_2Frame(wxWindow* parent,wxWindowID id)
     Button2 = new wxButton(this, ID_BUTTON2, _("A"), wxPoint(632,112), wxDefaultSize, 0, wxDefaultValidator, _T("ID_BUTTON2"));
     Button3 = new wxButton(this, ID_BUTTON3, _("B"), wxPoint(752,112), wxDefaultSize, 0, wxDefaultValidator, _T("ID_BUTTON3"));
     Button4 = new wxButton(this, ID_BUTTON4, _("C"), wxPoint(632,160), wxDefaultSize, 0, wxDefaultValidator, _T("ID_BUTTON4"));
-    Button5 = new wxButton(this, ID_BUTTON5, _("D"), wxPoint(752,160), wxDefaultSize, 0, wxDefaultValidator, _T("ID_BUTTON5"));
+    Button5 = new wxButton(this, ID_BUTTON5, _("K"), wxPoint(752,160), wxDefaultSize, 0, wxDefaultValidator, _T("ID_BUTTON5"));
     Button6 = new wxButton(this, ID_BUTTON6, _("E"), wxPoint(688,240), wxDefaultSize, 0, wxDefaultValidator, _T("ID_BUTTON6"));
     StaticText1 = new wxStaticText(this, ID_STATICTEXT1, _("Monitor de memoria"), wxPoint(664,64), wxDefaultSize, 0, _T("ID_STATICTEXT1"));
     StaticText2 = new wxStaticText(this, ID_STATICTEXT2, _("Puerto,IP server Activo"), wxPoint(24,40), wxDefaultSize, 0, _T("ID_STATICTEXT2"));
     StaticText3 = new wxStaticText(this, ID_STATICTEXT3, _("Puerto,IP server Pasivo:"), wxPoint(24,72), wxDefaultSize, 0, _T("ID_STATICTEXT3"));
-    TextCtrl1 = new wxTextCtrl(this, ID_TEXTCTRL1, _("Text"), wxPoint(328,32), wxDefaultSize, 0, wxDefaultValidator, _T("ID_TEXTCTRL1"));
-    TextCtrl2 = new wxTextCtrl(this, ID_TEXTCTRL2, _("Text"), wxPoint(328,80), wxDefaultSize, 0, wxDefaultValidator, _T("ID_TEXTCTRL2"));
-    TextCtrl3 = new wxTextCtrl(this, ID_TEXTCTRL3, _("Text"), wxPoint(168,168), wxDefaultSize, 0, wxDefaultValidator, _T("ID_TEXTCTRL3"));
-    TextCtrl4 = new wxTextCtrl(this, ID_TEXTCTRL4, _("Text"), wxPoint(168,216), wxDefaultSize, 0, wxDefaultValidator, _T("ID_TEXTCTRL4"));
+    IP_SA = new wxTextCtrl(this, ID_TEXTCTRL1, _("Text"), wxPoint(328,32), wxDefaultSize, 0, wxDefaultValidator, _T("ID_TEXTCTRL1"));
+    IP_SP = new wxTextCtrl(this, ID_TEXTCTRL2, _("Text"), wxPoint(328,80), wxDefaultSize, 0, wxDefaultValidator, _T("ID_TEXTCTRL2"));
+    Llave_1 = new wxTextCtrl(this, ID_TEXTCTRL3, _("Text"), wxPoint(168,168), wxDefaultSize, 0, wxDefaultValidator, _T("ID_TEXTCTRL3"));
+    Valor_1 = new wxTextCtrl(this, ID_TEXTCTRL4, _("Text"), wxPoint(168,216), wxDefaultSize, 0, wxDefaultValidator, _T("ID_TEXTCTRL4"));
     Button7 = new wxButton(this, ID_BUTTON7, _("Enviar"), wxPoint(168,264), wxDefaultSize, 0, wxDefaultValidator, _T("ID_BUTTON7"));
-    TextCtrl5 = new wxTextCtrl(this, ID_TEXTCTRL5, _("Text"), wxPoint(224,32), wxDefaultSize, 0, wxDefaultValidator, _T("ID_TEXTCTRL5"));
-    TextCtrl6 = new wxTextCtrl(this, ID_TEXTCTRL6, _("Text"), wxPoint(224,80), wxDefaultSize, 0, wxDefaultValidator, _T("ID_TEXTCTRL6"));
-    Valor = new wxStaticText(this, ID_STATICTEXT4, _("Memoria"), wxPoint(40,168), wxDefaultSize, 0, _T("ID_STATICTEXT4"));
-    StaticText5 = new wxStaticText(this, ID_STATICTEXT5, _("Valor"), wxPoint(48,224), wxDefaultSize, 0, _T("ID_STATICTEXT5"));
+    Puerto_SA = new wxTextCtrl(this, ID_TEXTCTRL5, _("Text"), wxPoint(224,32), wxDefaultSize, 0, wxDefaultValidator, _T("ID_TEXTCTRL5"));
+    Puerto_SP = new wxTextCtrl(this, ID_TEXTCTRL6, _("Text"), wxPoint(224,80), wxDefaultSize, 0, wxDefaultValidator, _T("ID_TEXTCTRL6"));
+    Valor = new wxStaticText(this, ID_STATICTEXT4, _("Llave"), wxPoint(40,168), wxDefaultSize, 0, _T("ID_STATICTEXT4"));
+    StaticText5 = new wxStaticText(this, ID_STATICTEXT5, _("Valor"), wxPoint(40,224), wxDefaultSize, 0, _T("ID_STATICTEXT5"));
+    Button8 = new wxButton(this, ID_BUTTON8, _("Eliminar"), wxPoint(168,312), wxDefaultSize, 0, wxDefaultValidator, _T("ID_BUTTON8"));
+    Button9 = new wxButton(this, ID_BUTTON9, _("Comparar"), wxPoint(256,360), wxDefaultSize, 0, wxDefaultValidator, _T("ID_BUTTON9"));
+    Memoria_2 = new wxTextCtrl(this, ID_TEXTCTRL7, _("Text"), wxPoint(336,168), wxDefaultSize, 0, wxDefaultValidator, _T("ID_TEXTCTRL7"));
+    Valor_2 = new wxTextCtrl(this, ID_TEXTCTRL8, _("Text"), wxPoint(336,216), wxDefaultSize, 0, wxDefaultValidator, _T("ID_TEXTCTRL8"));
     MenuBar1 = new wxMenuBar();
     Menu1 = new wxMenu();
     MenuItem1 = new wxMenuItem(Menu1, idMenuQuit, _("Quit\tAlt-F4"), _("Quit the application"), wxITEM_NORMAL);
@@ -127,11 +136,14 @@ Ui_2Frame::Ui_2Frame(wxWindow* parent,wxWindowID id)
     Center();
 
     Connect(ID_BUTTON1,wxEVT_COMMAND_BUTTON_CLICKED,(wxObjectEventFunction)&Ui_2Frame::OnButton1Click);
+    Connect(ID_BUTTON5,wxEVT_COMMAND_BUTTON_CLICKED,(wxObjectEventFunction)&Ui_2Frame::OnButton5Click);
     Connect(ID_BUTTON6,wxEVT_COMMAND_BUTTON_CLICKED,(wxObjectEventFunction)&Ui_2Frame::OnButton6Click);
     Connect(ID_TEXTCTRL4,wxEVT_COMMAND_TEXT_UPDATED,(wxObjectEventFunction)&Ui_2Frame::OnTextCtrl4Text);
     Connect(ID_BUTTON7,wxEVT_COMMAND_BUTTON_CLICKED,(wxObjectEventFunction)&Ui_2Frame::OnButton7Click);
+    Connect(ID_TEXTCTRL5,wxEVT_COMMAND_TEXT_UPDATED,(wxObjectEventFunction)&Ui_2Frame::OnTextCtrl5Text);
     Connect(idMenuQuit,wxEVT_COMMAND_MENU_SELECTED,(wxObjectEventFunction)&Ui_2Frame::OnQuit);
     Connect(idMenuAbout,wxEVT_COMMAND_MENU_SELECTED,(wxObjectEventFunction)&Ui_2Frame::OnAbout);
+    Connect(wxID_ANY,wxEVT_CLOSE_WINDOW,(wxObjectEventFunction)&Ui_2Frame::OnClose1);
     //*)
 }
 
@@ -166,13 +178,40 @@ void Ui_2Frame::OnTextCtrl4Text(wxCommandEvent& event)
 
 void Ui_2Frame::OnButton1Click(wxCommandEvent& event)
 {
-    rm_init("127.0.0.1",8654,"127.0.0.1",8790);
+    wxString mensaje=IP_SA->GetValue();
+    std::string ip_sa = std::string(mensaje.mb_str());//IP SA
+    wxString mensaje2=IP_SP->GetValue();
+    std::string ip_sp = std::string(mensaje2.mb_str());//IP SP
+    wxString mensaje3=Puerto_SA->GetValue();
+    std::string puerto_sa = std::string(mensaje3.mb_str());//Puerto SA
+    wxString mensaje4=Puerto_SP->GetValue();
+    std::string puerto_sp = std::string(mensaje4.mb_str());//Puerto SP
+
+
+    //pasa de string a char* los ip
+    char * ip1 = new char[ip_sa.size() + 1];
+    std::copy(ip_sa.begin(), ip_sa.end(), ip1);
+    ip1[ip_sa.size()] = '\0';
+    char * ip2 = new char[ip_sp.size() + 1];
+    std::copy(ip_sp.begin(), ip_sp.end(), ip1);
+    ip2[ip_sp.size()] = '\0';
+    // paso de string a int los puertos
+    int puerto1 = atoi(puerto_sa.c_str());
+    int puerto2 = atoi(puerto_sp.c_str());
+    rm_init(ip1,puerto1,ip2,puerto2);
 }
 
 void Ui_2Frame::OnButton7Click(wxCommandEvent& event)
 {
-    enviar(1,"hola",1024);
-    enviar(2,"hola",1024);
+    wxString mensaje=Llave_1->GetValue();
+    std::string mensaje_s = std::string(mensaje.mb_str());//Llave
+    wxString mensaje2=Valor_1->GetValue();
+    std::string mensaje_s2 = std::string(mensaje2.mb_str());//Valor
+
+    string dato_enviar="1"+mensaje_s+"_"+mensaje_s2+"*";
+
+    wxMessageBox(dato_enviar);
+    enviar(dato_enviar,1024);
 }
 
 void Ui_2Frame::OnButton6Click(wxCommandEvent& event)
@@ -183,3 +222,21 @@ void Ui_2Frame::OnButton6Click(wxCommandEvent& event)
     wxMessageBox(buffer);
 }
 
+
+void Ui_2Frame::OnTextCtrl5Text(wxCommandEvent& event)
+{
+}
+
+void Ui_2Frame::OnClose1(wxCloseEvent& event)
+{
+}
+
+void Ui_2Frame::OnButton5Click(wxCommandEvent& event)
+{
+    wxMessageBox("Quien dice? atte:Kelly \n pd:hola");
+}
+
+void Ui_2Frame::OnButton2Click(wxCommandEvent& event)
+{
+    wxMessageBox("Cache:A , Valor: mensaje \n Size: 8 \n Referencias: 1");
+}
--- Ui_2/rmlib.h
+++ Ui_2/rmlib.h
@@ -12,42 +12,38 @@
 #include <thread>
 using namespace std;
 
-int portNum ;
+int puerto_client ;
 int portNum2 ;
 int disponible_server_HA=0; //ASI VERIFICO SI ESTA CONECTADO O DESCONECTADO
 int disponible_server_PA=0;
-    int client; // socket file descriptors
-     // port number (same that server)
-
+int client; //socket cliente 1, osea el del servidor activo
 int bufsize = 1024; // buffer size
 char buffer[1024]; // buffer to transmit
-
 char ip[] = "127.0.0.1"; // Server IP
 bool isExit = false; // var fo continue infinitly
 string echo="";
 string echo2="";
-
-
-
-   /* -------------- INITIALIZING VARIABLES -------------- */
-    int client2; // socket file descriptors
-     // port number (same that server)
-
-    char buffer2[1024]; // buffer to transmit
-
-    bool isExit2 = false; // var fo continue infinitly
+int client2; //socket cliente 2, osea el del servidor pasivo
+char buffer2[1024]; // buffer to transmit
+bool isExit2 = false; // var fo continue infinitly
 
 
 string& bufferToString(char* buffer, int bufflen, string& str)
 {
     char temp[bufflen];
-
     memset(temp, '\0', bufflen + 1);
     strncpy(temp, buffer, bufflen);
-
     return(str.assign(temp));
 }
+/*
+Convierte un buffer en string
+*/
+struct rmRef_h{
+    string llave;
+    int Size;
+    string valor;
 
+};
 void peticion(int cliente_S,char B[],int size_B)
 {
     char msj[1024]="pt*";
@@ -56,49 +52,37 @@ void peticion(int cliente_S,char B[],int size_B)
     {
          send(client,msj,size_B,0);
          recv(client, buffer, bufsize, 0);
-
     }
     if (cliente_S==2)
     {
-
-
         send(client2,msj,size_B,0);
         recv(client2, buffer2, bufsize, 0);
-
-
     }
 
 
 }
+/*
+envia un mensaje a un servidor y espera su respuesta
+*/
 
-void enviar(int cliente_S,char B[],int size_B)
+void enviar(string data,int size_B)
 {
-    char msj[1024]="hola1*";
-    char msj2[1024]="hola2*";
-    if (cliente_S==1)
-    {
-        send(client,msj,size_B,0);
-    }
-    if (cliente_S==2)
-    {
-        send(client2,msj2,size_B,0);
-    }
 
-
+    char string_array[1024];
+    strcpy(string_array, data.c_str());
+    send(client,string_array,size_B,0);
 
 }
+/*
+envia un mensaje a un servidor
+*/
 void cliente(){
-/* -------------- INITIALIZING VARIABLES -------------- */
-
 
     /* Structure describing an Internet socket address. */
-struct sockaddr_in server_addr;
-
+    struct sockaddr_in server_addr;
 
     /* ---------- ESTABLISHING SOCKET CONNECTION ----------*/
-
     client = socket(AF_INET, SOCK_STREAM, 0);
-
     /*
     * The socket() function creates a new socket.
     * It takes 3 arguments:
@@ -108,15 +92,11 @@ struct sockaddr_in server_addr;
     * 3) Third is a protocol argument: should always be 0.
     * If the socket call fails, it returns -1.
     */
-
     if (client < 0)
             {
 
         exit(-1);
             }
-
-
-
     /*
     * The variable serv_addr is a structure of sockaddr_in.
     * sin_family contains a code for the address family.
@@ -127,48 +107,26 @@ struct sockaddr_in server_addr;
     * htons() converts the port number from host byte order
     * to a port number in network byte order.
     */
-
     server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(portNum);
-
+    server_addr.sin_port = htons(puerto_client);
     /*
     * This function converts an Internet address (either IPv4 or IPv6)
     * from presentation (textual) to network (binary) format.
     * If the comunication is on the same machine, you can comment this line.
     */
     inet_pton(AF_INET, ip, &server_addr.sin_addr);
-
-    /* ---------- CONNECTING THE SOCKET ---------- */
-
     if (connect(client, (struct sockaddr *) &server_addr, sizeof(server_addr))
         < 0)
-
-    /*
-    * The connect function is called by the client to
-    * establish a connection to the server. It takes
-    * three arguments, the socket file descriptor, the
-    * address of the host to which it wants to connect
-    * (including the port number), and the size of this
-    * address.
-    * This function returns 0 on success and -1
-    * if it fails.
-    * Note that the client needs to know the port number of
-    * the server but not its own port number.
-    */
-
-
     // recive the welcome message from server
    // recv(client, buffer, bufsize, 0);
-
-
     // loop to send messages to server
     disponible_server_HA=1;
-
-
-
+    send(client,"SA",1024,0);
 
 }
-
+/*
+Permite conectarme con el servidor activo
+*/
 
 /***************************************************************************************/
 /***************************************************************************************/
@@ -249,28 +207,35 @@ void cliente2(){
 
 
         disponible_server_PA=0;
+        send(client2,"SP",1024,0);
 }
+/*
+permite conectarme con el servidor pasivo
+*/
+void rm_init(char* ip,int port,char* ipHA,int portHA)
+{
 
-void rm_init(char* ip,int port,char* ipHA,int portHA){
-    portNum=port;
+    puerto_client=port;
     portNum2=portHA;
     thread t1(cliente); // para conectarse con el server activo
     thread t2(cliente2);
     t1.join();
     t2.join();
-    //t2.join();
-    //t3.join();
-
-
-
 
 }
 
-void rm_new(char* key,void* value,int value_size){
+void rm_new(char* key,void* value,int value_size)
+{
 
 }
 
-void rm_delete(char* msj){
+void rm_delete(char* msj)
+{
+
+}
+rmRef_h rm_get(char* key){
+    rmRef_h resultado;
+    return resultado;
 
 }
 
